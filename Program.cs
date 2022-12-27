9d171d27d687b2c4e194403410eee02e7d8e67d2
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-12-27 14:05:53 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "Welcome to Acme Accounting Systems" + " Remember, we're \"accounting\" on you!";
+            Console.WriteLine(txt);
+        }
+    }
+}
