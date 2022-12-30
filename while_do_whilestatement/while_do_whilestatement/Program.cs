a5ad68716ffe866d0355dfbbdd0c6b71b9047b9b
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-12-30 11:42:21 -0700

    Add files via upload

---
+++ while_do_whilestatement/while_do_whilestatement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace while_do_whilestatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int x = 0 adds until it is less than 5 then the while loop stops
+            int x = 0;
+            while (x < 5)
+            {
+                Console.WriteLine(x);
+                x++;
+               
+            }
+
+            {
+                //// The do while loop stops execution exits when a boolean condition evaluates to false
+                {
+                    int y = 0;
+                    do
+                    {
+                        Console.WriteLine(y);
+                        y++;
+                    } while (y < 10);
+
+                }
+            }
+        }
+    }
+}
