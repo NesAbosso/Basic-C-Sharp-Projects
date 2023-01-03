62c7a8bd3446b3852edcfa8b031a774279eaf4d4
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-03 16:24:35 -0700

    Add files via upload

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethodsAssignment
+{
+    public class MathOperations
+    {
+        // This method multiplies the input by 2
+        public int MultiplyByTwo(int input)
+        {
+            return input * 2;
+        }
+
+        // This method divides the input by 3
+        public int DivideByThree(int input)
+        {
+            return input / 3;
+        }
+
+        // This method adds 5 to the input
+        public int AddFive(int input)
+        {
+            return input + 5;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.WriteLine("Enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call the MultiplyByTwo method and display the result
+            int result1 = mathOps.MultiplyByTwo(userInput);
+            Console.WriteLine(result1);
+
+            // Call the DivideByThree method and display the result
+            int result2 = mathOps.DivideByThree(userInput);
+            Console.WriteLine(result2);
+
+            // Call the AddFive method and display the result
+            int result3 = mathOps.AddFive(userInput);
+            Console.WriteLine(result3);
+        }
+    }
+}
