daead050312851828118ee8ba3ba9801d52b6de5
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-03 18:24:05 -0700

    Add files via upload

---
+++ MethodClass/MethodClass/MClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClass
+{
+    public class MathOperation
+    {
+        public void DoMath(int num1, int num2)
+        {
+            // Perform some math operation on num1
+            num1 *= 2;
+
+            // Display num2 to the screen
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperation class
+            MathOperation mathOp = new MathOperation();
+
+            // Call the DoMath() method and pass in two numbers
+            mathOp.DoMath(10, 20);
+
+            // Call the DoMath() method and specify the parameters by name
+            mathOp.DoMath(num2: 30, num1: 40);
+        }
+    }
+}
