338250e16766bb6314628afb50024dfa1d34af37
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-04 15:05:43 -0700

    added

---
+++ Methods_and_Objects/Methods_and_Objects/persons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_and_Objects
+{
+    //class called persons and give it two properties
+    class persons
+    {
+        //two properties, each of data type string
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //this class a void method called SayName()
+        //writes the person's full name to the console in the format of: “Name: [full name]”.
+        public void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+
+    //Employess class inherits from the persons class
+    class Employee : persons
+    {
+        //property called Id and data type int
+        public int Id { get; set; }
+    }
+}
+
