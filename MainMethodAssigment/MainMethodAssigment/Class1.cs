639733bbe93bb45b6db60a2700551db3b9d939eb
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-03 17:25:02 -0700

    Add files via upload

---
+++ MainMethodAssigment/MainMethodAssigment/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssigment
+{
+    class MathOperations
+    {
+
+            // Method that takes in an integer and returns the result of a math operation
+        public int Calculate(int num)
+        {
+            // You can choose any math operation you like
+            int result = num + 10;
+            return result;
+        }
+
+        // Method that takes in a decimal and returns the result of a math operation
+        public int Calculate(decimal num)
+        {
+            // You can choose any math operation you like
+            int result = (int)(num / 2);
+            return result;
+        }
+
+        // Method that takes in a string and returns the result of a math operation
+        public int Calculate(string str)
+        {
+            // Try to convert the string to an integer
+            if (int.TryParse(str, out int num))
+            {
+                // You can choose any math operation you like
+                int result = num * 10;
+                return result;
+            }
+            else
+            {
+                // If the string could not be converted to an integer, return 0
+                return 0;
+            }
+        }
+    }
+
+}
---
+++ MainMethodAssigment/MainMethodAssigment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MainMethodAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOp = new MathOperations();
+
+            // Call the Calculate method with an integer argument
+            int result1 = mathOp.Calculate(4);
+            Console.WriteLine(result1);
+
+            // Call the Calculate method with a decimal argument
+            int result2 = mathOp.Calculate(2.14m);
+            Console.WriteLine(result2);
+
+            // Call the Calculate method with a string argument
+            int result3 = mathOp.Calculate("6");
+            Console.WriteLine(result3);
+        }
+    }
+}
