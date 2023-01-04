274aa9dc091977e65ea395f141c87b5c27d50785
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-03 18:05:55 -0700

    Add files via upload

---
+++ Methods/Methods/Classm1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class MathOperation
+    {
+        public int DoMath(int num1, int num2 = 0)
+        {
+            // Perform some math operation using num1 and num2
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperation class
+            MathOperation mathOp = new MathOperation();
+
+            // Ask the user to enter two numbers
+            Console.WriteLine("Enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number (optional): ");
+            string input = Console.ReadLine();
+
+            int num2 = 0;
+            if (!string.IsNullOrEmpty(input))
+            {
+                num2 = int.Parse(input);
+            }
+
+            // Call the DoMath() method and pass in num1 and num2
+            int result = mathOp.DoMath(num1, num2);
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
