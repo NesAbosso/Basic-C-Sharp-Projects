ad2e4efebcacc2728cc3e261ae9afa27544f477a
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-04 16:02:17 -0700

    upload

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate an Employee object with firstName “Sample” and lastName “Student
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+            //Call the SayName() method on the object.
+            employee.SayName();
+        }
+        }
+    }
+    
---
+++ AbstractClass/AbstractClass/person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+
+    //abstract class called Person
+    abstract class Person
+        {
+        //two properties: string firstName and string lastName
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //adstract method SayName().
+        public abstract void SayName();
+        }
+
+    //Employee class inherits from Person class
+        class Employee : Person
+        {
+        //SayName() method inside of the Employee class
+        //with override
+        public override void SayName()
+            {
+                Console.WriteLine($"Name: {FirstName} {LastName}");
+            }
+        }
+    }
+
+
