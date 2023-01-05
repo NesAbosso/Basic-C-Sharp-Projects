be0a2182246cddf7e11a09abc6e35e52bfd22439
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-05 16:20:50 -0700

    Add files via upload

---
+++ Parameters/Parameters/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parameters
+{
+    //Employee class with a generic type parameter
+    public class Employee<T>
+    {
+        //added a property to employee class things with a 
+        //data type generic list
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<T> Things { get; set; }
+    
+    }
+
+
+}
---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parameters
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assign a list of strings as the property value of Things.
+            Employee<string> e1 = new Employee<string>
+            {
+                Id = 1,
+                FirstName = "Jared",
+                LastName = "Stephens",
+                Things = new List<string> { "Jeep", "Dodge", "Nissan" }
+            };
+
+            //Assign a list of integers as the property value of Things.
+            Employee<int> e2 = new Employee<int>
+            {
+                Id = 2,
+                FirstName = "Jolie",
+                LastName = "Forsyth",
+                Things = new List<int> { 1, 2, 3 }
+            };
+
+            //Create a loop that prints all of the Things to the Console
+            foreach (var thing in e1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (var thing in e2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
+
+ 
+    
+    
+
+
