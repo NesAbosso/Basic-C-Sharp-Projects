5409afd866f386fcb56323e96fa547ef5b4b3b7f
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-05 15:47:24 -0700

    upload

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators
+{
+    //Create an Employee class with Id, FirstName and LastName properties
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by
+        //comparing their Id property
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+        public override bool Equals(object obj)
+        {
+            var employee = obj as Employee;
+            return employee != null && Id == employee.Id;
+        }
+
+        //override the Equals method and the GetHashCode method if you want to use the Equals method to
+        //compare two Employee objects
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate two objects of the Employee class and
+            //assign values to their properties
+            Employee employee1 = new Employee
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            Employee employee2 = new Employee
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Doe"
+            };
+            //Then compare the two Employee objects using the newly overloaded operators
+            // and display the results
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("The two employees have the same Id.");
+            }
+            else
+            {
+                Console.WriteLine("The two employees have different Ids.");
+            }
+        }
+    }
+}
