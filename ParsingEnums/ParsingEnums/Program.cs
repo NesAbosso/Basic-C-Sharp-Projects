d85aaefab5d80994fea87163db8ef726896ec6e3
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-05 17:17:43 -0700

    uploaded

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt the user to enter the current day of the week
+            Console.WriteLine("Enter the current day of the week:");
+
+            //Wrap the above statement in a try/catch block and have it print "Please enter an
+            //actual day of the week.” to the console if an error occurs.
+            try
+            {
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+                Console.WriteLine($"Today is {day}.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
---
+++ ParsingEnums/ParsingEnums/WeekDays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParsingEnums
+{
+    public class WeekDays
+    {
+        //Create an enum for the days of the week with value
+        enum DayOfWeek
+        {
+            Monday = 2,
+            Tuesday = 3,
+            Wednesday = 4,
+            Thursday = 5,
+            Friday = 6,
+            Saturday = 7,
+            Sunday = 1
+        }
+
+    }
+}
