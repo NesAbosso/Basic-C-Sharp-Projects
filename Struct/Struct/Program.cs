6e440978df8d1ad39c5bf9cfefbab5d091ce6216
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-05 17:45:41 -0700

    upload

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+
+        //Create a struct called Number and give it the property
+        //“Amount” and have it be of data type decimal.
+        struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+        //In the Main() method, create an object of
+        //data type Number and assign an amount to it.
+        static void Main(string[] args)
+        {
+            Number n = new Number { Amount = 400.55m };
+            Console.WriteLine(n.Amount);
+        }
+
+    }
+}
+
