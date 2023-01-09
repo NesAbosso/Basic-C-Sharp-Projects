61cadb59bda1ee499da79ef072462fdd0aafbb86
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-09 16:07:27 -0700

    upload

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DateTimeNow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time to the console
+            Console.WriteLine(DateTime.Now);
+
+            // Ask the user for a number
+            Console.Write("Enter a number of hours: ");
+            string input = Console.ReadLine();
+            int hours = int.Parse(input);
+
+            // Print the exact time it will be in X hours
+            DateTime future = DateTime.Now.AddHours(hours);
+            Console.WriteLine($"In {hours} hours, it will be {future:t}.");
+        }
+    }
+}
