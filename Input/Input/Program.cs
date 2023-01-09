e034057b5a3d65a15b1f7a73da4dfae5939a434b
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-09 15:38:05 -0700

    upload

---
+++ Input/Input/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            // Log the number to a text file
+            string filePath = "numbers.txt";
+            File.AppendAllText(filePath, number + Environment.NewLine);
+
+            // Print the text file back to the user
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
