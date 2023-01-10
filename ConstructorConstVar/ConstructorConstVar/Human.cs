04285a5c20e66a6555a52d9c66d10cf22d7df024
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-01-09 17:33:52 -0700

    upload

---
+++ ConstructorConstVar/ConstructorConstVar/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorConstVar
+{
+    public class Human
+    {
+        //create human class with name and age
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Human(string name) : this(name, 0)
+        {
+        }
+
+        public Human(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ ConstructorConstVar/ConstructorConstVar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorConstVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a const variable.
+            const int maxAttempts = 1;
+            for (int i = 1; i <= maxAttempts; i++)
+            {
+                Console.WriteLine($"Attempt {i} of {maxAttempts}");
+                {
+                    //Create a variable using the keyword “var"
+                    var name = "Jared";
+                    Console.WriteLine($"Name: {name}");
+
+                    var person = new Human("Stephens", 49);
+                    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+                }
+            }
+        }
+    }
+}
